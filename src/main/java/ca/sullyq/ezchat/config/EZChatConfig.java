898c08b3bafd3dd53d2e83f3cd08c29dcf4eb88a
Sully <daltonpineault@gmail.com>
2026-02-05 10:05:58 -0500

    Refactored TagConfig -> EZChatConfig

--- src/main/java/ca/sullyq/ezchat/config/EZChatConfig.java
+++ src/main/java/ca/sullyq/ezchat/config/EZChatConfig.java
@@ -14,6 +14,7 @@ import java.util.Map;
 public class TagConfig {
 
     private Map<String, String> tags = new HashMap<>();
+    private Map<String, String> chatFormats = new HashMap<>();
 
     public TagConfig() {
 //        this.playerTags.put("[Admin]", "#f84848");
